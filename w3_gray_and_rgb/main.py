d0adbf6bb6dacf6b9159fb3e194e25a6433b38af
Ahmet Eren Tomurcuk <54647908+erentomurcuk@users.noreply.github.com>
2022-10-26 19:21:07 +0300

    Initial commit

---
+++ w3_gray_and_rgb/main.py
@@ -0,0 +1,38 @@
+import numpy as np
+import cv2 as cv
+
+blue64 = np.zeros((64,64,3),np.uint8)
+green64 = np.zeros((64,64,3),np.uint8)
+red64 = np.zeros((64,64,3),np.uint8)
+
+# Importing the image
+_64pimg = cv.imread('64pimg.png')
+
+# Convert the image to a grayscale image
+_64pimg_grayscale = cv.cvtColor(_64pimg, cv.COLOR_BGR2GRAY)
+
+# Save the monochrome image
+cv.imwrite("64p_monochrome.png", _64pimg_grayscale)
+
+# Extract colours B, G, R
+_64pimg_blue = _64pimg[:,:,0]
+_64pimg_green = _64pimg[:,:,1]
+_64pimg_red = _64pimg[:,:,2]
+
+# Create single colour images
+for x in range (64):
+    for y in range (64):
+        blue64[x,y,0] = _64pimg_blue[x,y]
+        green64[x,y,1] = _64pimg_green[x,y]
+        red64[x,y,2] = _64pimg_red[x,y]
+
+
+# Save monochrome single colour images
+cv.imwrite("64p_blue.png", _64pimg_blue)
+cv.imwrite("64p_green.png", _64pimg_green)
+cv.imwrite("64p_red.png", _64pimg_red)
+
+# Save coloured single colour images
+cv.imwrite("64p_blue_clr.png", blue64)
+cv.imwrite("64p_green_clr.png", green64)
+cv.imwrite("64p_red_clr.png", red64)
